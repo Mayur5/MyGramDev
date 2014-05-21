581448e01022c590f2a17b9589256ce0dc2e6436
Ramshreyas Rao <ramshreyas.rao@gmail.com>
2014-05-21 19:15:33 +0530

    Barebones inbox ui implemented.

--- MygramMockUp/src/com/example/mygrammockup/MainActivity.java
+++ MygramMockUp/src/com/example/mygrammockup/MainActivity.java
@@ -1,46 +1,58 @@
 package com.example.mygrammockup;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
-	protected static Cursor inbox;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
+		//Fetch Inbox emails
+		final String[] inboxArray = fetchMail();
+		
+		///Populate Inbox ListView
+		final ListView inboxView = (ListView)findViewById(R.id.inboxView);
+		final InboxAdapter adapter = new InboxAdapter(this, inboxArray);
+		inboxView.setAdapter(adapter);
+		
+		//Set clickListener
+		inboxView.setOnItemClickListener(new OnItemClickListener() {
+			
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				// Get the text
+				String snippet = (String)inboxView.getAdapter().getItem(position);		
+				Toast.makeText(getApplicationContext(), snippet, Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private String[] fetchMail() {
+		String[] threads = new String[] {
+				"Oi! Call when you can",
+				"9863647272",
+				"Thanks - that was great fun...",
+				"Call when you're free pls - it's urgent.",
+				"Thank you",
+				"arumugam@seventyfive.com",
+				"lol",
+				"Bring some salt also.",
+				"You have won 10,000 rupees in our...",
+				"Hey what's srivats's number?",
+				"=)"
+		};
+		
+		return threads;
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
+	
+	
 }
