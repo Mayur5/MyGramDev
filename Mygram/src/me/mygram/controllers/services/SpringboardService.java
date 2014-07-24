2268e003cd39e8fe1f7f396ea9f99fad52ac8129
Ramshreyas Rao <ramshreyas.rao@gmail.com>
2014-07-24 12:50:49 +0530

    Incremental

--- Mygram/src/me/mygram/controllers/services/SpringboardService.java
+++ Mygram/src/me/mygram/controllers/services/SpringboardService.java
@@ -23,11 +23,13 @@ public class SpringboardService implements GenericSpringboardService{
 		final int instagram_icon=R.drawable.instagram_icon;
 		final int irctc_icon=R.drawable.irctc;
 		final int flipkart_icon=R.drawable.flipkart;
+		final int krishi_icon=R.drawable.krishi_soochana;
+		final int mandi_icon=R.drawable.mandi;
 		
 		
 		roster.add(new Vendor("Flipkart").setIcon(flipkart_icon).setDescription("Everything from Fashion to \nCellphones and electronics!").setUrl("file:///android_asset/flipkart.html"));
-		roster.add(new Vendor("IRCTC").setIcon(irctc_icon).setDescription("Book train tickets \non Indian Railways").setUrl("file:///android_asset/krishiSoochana.html"));
-		roster.add(new Vendor("Instagram").setIcon(instagram_icon).setDescription("This is a description \nof instagram").setUrl("file:///android_asset/mandi.html"));
+		roster.add(new Vendor("Krishi").setIcon(krishi_icon).setDescription("Information for farmers").setUrl("file:///android_asset/krishiSoochana.html"));
+		roster.add(new Vendor("Mandi").setIcon(mandi_icon).setDescription("Vegetable auctions for small-holdings").setUrl("file:///android_asset/mandi.html"));
 		roster.add(new Vendor("Dropbox").setIcon(dropbox_icon).setDescription("This is a description of dropbox").setUrl("file:///android_asset/jobPortal.html"));
 		roster.add(new Vendor("Blur").setIcon(blur_icon).setDescription("This is a description \nof blur").setUrl("file:///android_asset/flipkart.html"));
 		roster.add(new Vendor("Amazon").setIcon(amazon_icon).setDescription("This is a description \nof amazon").setUrl("file:///android_asset/vendorLogin.html"));
