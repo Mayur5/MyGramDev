6c0e215c44a8641677052122e45a881503eaae51
Ramshreyas Rao <ramshreyas.rao@gmail.com>
2014-07-24 09:27:18 +0530

    Incremental Commit

--- Mygram/src/me/mygram/example/client/MySpringboardActivity.java
+++ Mygram/src/me/mygram/example/client/MySpringboardActivity.java
@@ -3,15 +3,18 @@ package me.mygram.example.client;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import me.mygram.R;
+import me.mygram.models.Credentials;
 import me.mygram.views.MyActivity;
 
 public class MySpringboardActivity extends MyActivity {
+	Credentials credentials;
 
 	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+		credentials = getCredentials();
 		super.onCreateParentMethod(savedInstanceState);
 		setContentView(R.layout.activity_myspringboard);
 		
@@ -21,6 +24,43 @@ public class MySpringboardActivity extends MyActivity {
 		//Instantiate webView
 		WebView webView = (WebView)findViewById(R.id.springboard_web_view);
 		webView.getSettings().setJavaScriptEnabled(true);
+		webView.setWebViewClient(new WebViewClient() {
+		    public void onPageFinished(WebView view, String url) {
+		        view.loadUrl("javascript:" + 
+		        			"document.getElementById('firstname').value = '" + credentials.getFirstName() + "';" +
+		        			"document.getElementById('firstName').value = '" + credentials.getFirstName() + "';" +
+		        			"document.getElementById('first_name').value = '" + credentials.getFirstName() + "';" +
+		        			"document.getElementById('FirstName').value = '" + credentials.getFirstName() + "';" +
+		        			"document.getElementById('first').value = '" + credentials.getFirstName() + "';" +
+		        			
+							"document.getElementById('username').value = '" + credentials.getUserName() + "';" +
+							"document.getElementById('user_name').value = '" + credentials.getUserName() + "';" +			
+							"document.getElementById('userName').value = '" + credentials.getUserName() + "';" +
+		        			
+		        			"document.getElementById('lastname').value='"+credentials.getLastName() + "';" + 
+		        			"document.getElementById('lastName').value='"+credentials.getLastName() + "';" + 
+		        			"document.getElementById('last_name').value='"+credentials.getLastName() + "';" + 
+		        			"document.getElementById('LastName').value='"+credentials.getLastName() + "';" + 
+		        			"document.getElementById('last').value='"+credentials.getLastName() + "';" + 
+		        			
+		        			"document.getElementById('dob').value='"+credentials.getDateOfBith() +"';" + 
+		        			"document.getElementById('DOB').value='"+credentials.getDateOfBith() +"';" + 
+		        			"document.getElementById('dateOfBirth').value='"+credentials.getDateOfBith() +"';" + 
+		        			"document.getElementById('date_of_birth').value='"+credentials.getDateOfBith() +"';" +
+		        			"document.getElementById('dateofbirth').value='"+credentials.getDateOfBith() +"';" + 
+		        			
+							"document.getElementById('email').value='"+credentials.getEmailID() +
+							"document.getElementById('emailAddress').value='"+credentials.getEmailID() +
+							"document.getElementById('emailID').value='"+credentials.getEmailID() +
+							"document.getElementById('email_address').value='"+credentials.getEmailID() +
+							"document.getElementById('email_id').value='"+credentials.getEmailID() +
+		        			
+		        			"document.getElementById('phone').value='"+credentials.getPhoneNumber() +
+		        			"document.getElementById('phoneNumber').value='"+credentials.getPhoneNumber() +
+		        			"document.getElementById('phonenumber').value='"+credentials.getPhoneNumber() +
+		        			"document.getElementById('phone_number').value='"+credentials.getPhoneNumber() + "';");
+		    }
+		});
 		webView.loadUrl(url);
 	}
 
